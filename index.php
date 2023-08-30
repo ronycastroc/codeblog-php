47de09859cf5fa16b7406a5d907a871f512afc99
ronycastroc <castroroony@gmail.com>
2023-08-30 17:09:50 -0300

    style: home page css

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   include_once("templates/header.php");
 ?>
   <main>
@@ -10,19 +10,19 @@
       <?php foreach($posts as $post): ?>
         <div class="post-box">
           <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
-        </div>
-        <h2 class="post-title">
-          <a href="<?= $BASE_URL ?>/post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
-        </h2>
-        <p class="post-description"><?= $post['description'] ?></p>
-        <div class="tags-container">
-          <?php foreach($post['tags'] as $tag): ?>
-            <a href="#"><?= $tag ?></a>
-          <?php endforeach; ?>
+          <h2 class="post-title">
+            <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+          </h2>
+          <p class="post-description"><?= $post['description'] ?></p>
+          <div class="tags-container">
+            <?php foreach($post['tags'] as $tag): ?>
+              <a href="#"><?= $tag ?></a>
+            <?php endforeach; ?>
+          </div>
         </div>
       <?php endforeach; ?>
-    </div>    
+    </div>
   </main>
-<?php 
-  include_once("templates/footer.php");
+<?php
+  include_once("templates/footer.php")
 ?>
