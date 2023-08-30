3a009aa7545a1ef13344a290206599053d395770
ronycastroc <castroroony@gmail.com>
2023-08-30 12:26:17 -0300

    init config

---
+++ data/posts.php
@@ -0,0 +1,32 @@
+<?php
+
+  $posts = [
+    [
+      'id' => 1,
+      'title' => 'Programming in PHP',
+      'description' => 'In this post you will learn everything you need to be a good PHP programmer!',
+      'tags' => ['programming', 'PHP', 'programming logic'],
+      'img' => 'prog-1.jpg'
+    ],
+    [
+      'id' => 2,
+      'title' => 'Object Orientation with Java',
+      'description' => 'Learn the object-oriented paradigm with Java, a language on the rise.',
+      'tags' => ['Java', 'OOP', 'Advanced Programming'],
+      'img' => 'prog-2.jpg'
+    ],
+    [
+      'id' => 3,
+      'title' => 'The importance of programming logic',
+      'description' => 'Logic programming is the starting point for many programming students, is it important?',
+      'tags' => ['logic', 'programming', 'algorithms'],
+      'img' => 'prog-3.jpg',
+    ],
+    [
+      'id' => 4,
+      'title' => 'Programming in PHP',
+      'description' => 'In this post you will learn everything you need to be a good PHP programmer!',
+      'tags' => ['programming', 'PHP', 'programming logic'],
+      'img' => 'prog-4.jpg'
+    ],
+  ];
